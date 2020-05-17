b6c1bd282879ae97a58a86585b14adca9ab7653d
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-17 11:17:09 -0500

    Pull to refresh

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -51,16 +51,19 @@ class _ListPageState extends State<ListPage> {
   }
 
   _crearLista() {
-    return ListView.builder(
-      controller: _scrollController,
-      itemCount: _listaNumeros.length,
-      itemBuilder: (BuildContext context, int index) {
-        final imagen = _listaNumeros[index];
-        return FadeInImage(
-          image: NetworkImage('https://picsum.photos/500/300?image=$imagen'),
-          placeholder: AssetImage('assets/fade.gif'),
-        );
-      },
+    return RefreshIndicator(
+      onRefresh: obtenerPagina1,
+      child: ListView.builder(
+        controller: _scrollController,
+        itemCount: _listaNumeros.length,
+        itemBuilder: (BuildContext context, int index) {
+          final imagen = _listaNumeros[index];
+          return FadeInImage(
+            image: NetworkImage('https://picsum.photos/500/300?image=$imagen'),
+            placeholder: AssetImage('assets/fade.gif'),
+          );
+        },
+      ),
     );
   }
 
@@ -70,6 +73,16 @@ class _ListPageState extends State<ListPage> {
       _listaNumeros.add(_ultimoItem);
     }
     setState(() {});
+  }
+
+  Future<Null> obtenerPagina1() async {
+    final duration = new Duration(seconds: 2);
+    new Timer(duration, () {
+      _listaNumeros.clear();
+      _ultimoItem++;
+      _agregar10();
+    });
+    return Future.delayed(duration);
   }
 
   Future<Null> fetchData() async {
