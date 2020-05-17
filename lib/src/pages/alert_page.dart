553a24f3fe8dd962e7003edc1c87fe19f8ca0fdc
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-16 22:28:37 -0500

    Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,12 +7,44 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlert.call(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.pop(context);
         },
         child: Icon(Icons.access_alarm),
       ),
+    );
+  }
+
+  void _mostrarAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('Este es el contrenido de la caja de alerta'),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancelar')),
+            FlatButton(onPressed: () => Navigator.of(context).pop(), child: Text('Ok')),
+          ],
+        );
+      },
     );
   }
 }
