d48f5881193b72a5a13180557e0f64b3d0e8759c
Marco Yuquilima <myuquilima@engicoders.com>
2020-05-16 22:36:29 -0500

    Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,28 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://i.pinimg.com/originals/d3/72/4d/d3724dd8a0cc101e5faed272c1ad19c9.png'),
+              radius: 25,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/fade.gif'),
+          image: NetworkImage('https://venezuelaaldia.com/wp-content/uploads/2019/12/pocoy%C3%B3a.jpeg'),
+        ),
       ),
     );
   }
